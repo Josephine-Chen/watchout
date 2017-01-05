6c2966f463d5d64487fea54f34f7bce432bed93a
Josephine Chen <jychen@g.hmc.edu>
2017-01-04 22:02:03 -0800

    enemies created

--- client/watchout.js
+++ client/watchout.js
@@ -1,1 +1,32 @@
-// start slingin' some d3 here.
+//Static variables
+var static = {
+  windowX: window.innerWidth,
+  windowY: window.innerHeight,
+  enemyNum: 10,
+  enemyRadius: 10,
+  enemyColor: '#00F'
+}
+
+
+//Random
+var rand = function(n) {
+  return Math.floor(Math.random()*n);
+};
+var randX = rand(static.windowX);
+var randY = rand(static.windowY);
+
+//Create a board
+var board = d3.select('.board').append('svg')
+              .attr('width', static.windowX)
+              .attr('height', static.windowY);
+
+//Create some enemies
+var enemies = board.selectAll('circle')
+                    .data(d3.range(static.enemyNum)) //data to bind
+                    .enter() //create placeholder
+                    .append('circle') //append each circle to svg
+                    .attr('class', 'enemy') //enemy class
+                    .attr('cx', randX ) //random X position
+                    .attr('cy', randY ) //random Y position
+                    .attr('r', static.enemyRadius) //radius
+                    .attr('fill', static.enemyColor); //color
